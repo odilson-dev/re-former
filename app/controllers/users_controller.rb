7c99521efd37b0d22cd3a4e17b928c6ce687778f
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-13 12:11:54 -0500

    Build the html form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,23 @@ class UsersController < ApplicationController
     end
 
     def create
+        # @users = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+        @users = User.new(user_params)
+
+        if @users.save
+            redirect_to new_user_path
+        else
+            render :new, statuse: :unprocessable_entity
+        end
+
+    end
+
+    private
+
+    def user_params
+
+        params.require(:user).permit(:username, :email, :password)
+
     end
 end
