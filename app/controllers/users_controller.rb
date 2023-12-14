a886d5aa268fde1a773e70c11184f41c271003f2
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-14 10:15:07 -0500

    Editing and adding error display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,29 @@ class UsersController < ApplicationController
     end
 
     def create
-        # @users = User.new(username: params[:username], email: params[:email], password: params[:password])
 
-        @users = User.new(user_params)
+        @user = User.new(user_params)
 
-        if @users.save
+        if @user.save
             redirect_to new_user_path
         else
-            render :new, statuse: :unprocessable_entity
+            render :new, status: :unprocessable_entity
         end
 
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+    
+        if @user.update(user_params)
+          redirect_to @user
+        else
+          render :edit, status: :unprocessable_entity
+        end
     end
 
     private
